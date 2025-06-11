93a843da4e6d19f3b7aefac7d095a4c03ae3c90b
Wasiq Manzoor <164636793+Wasiqmanzoor90@users.noreply.github.com>
2025-06-11 23:53:08 +0530

    m2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
-import MainInvoice from "./invoice/main/page";
 
+import Register from "./user/register/page";
+import Login from "./user/login/page";
 export default function page(){
   return(
-    <MainInvoice/>
+    // <MainInvoice/>
+    <Login/>
   )
 }
