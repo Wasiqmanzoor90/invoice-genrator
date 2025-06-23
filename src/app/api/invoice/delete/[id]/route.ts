274cdc4a90ea72cdb258a9219e17dc92fd29aec4
Wasiq Manzoor <164636793+Wasiqmanzoor90@users.noreply.github.com>
2025-06-23 16:11:44 +0530

    oz

--- src/app/api/invoice/delete/[id]/route.ts
+++ src/app/api/invoice/delete/[id]/route.ts
@@ -4,11 +4,11 @@ import invoice from "../../../../../../model/invoice";
 
 export async function POST(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  context: { params: { id: string } }
 ) {
   try {
     await dbConnect();
-    const { id } = params;
+    const { id } = context.params;
 
     const existuser = await invoice.findById(id);
     if (!existuser) {
