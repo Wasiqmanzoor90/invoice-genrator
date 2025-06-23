558cbd1eabf2302945311edd822a40f3e26a2fc1
Wasiq Manzoor <164636793+Wasiqmanzoor90@users.noreply.github.com>
2025-06-23 11:15:26 +0530

    k1

--- lib/dbConnect.ts
+++ lib/dbConnect.ts
@@ -1,16 +1,15 @@
 import mongoose from "mongoose";
 
 export const dbConnect = async () => {
-    if(mongoose.connections[0].readyState )
-    {
-        console.log("already connected to db")
-        
-    }
-    try{
-        await mongoose.connect("mongodb://localhost:27017/PdfDb");
-        console.log("connected to db");
-    }
-    catch(error) {
-        console.log(error);
-    }
-}
+  if (mongoose.connection.readyState === 1) {
+    console.log("Already connected to DB");
+    return;
+  }
+
+  try {
+    await mongoose.connect(process.env.MONGO_URI!);
+    console.log("Connected to DB");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+};
